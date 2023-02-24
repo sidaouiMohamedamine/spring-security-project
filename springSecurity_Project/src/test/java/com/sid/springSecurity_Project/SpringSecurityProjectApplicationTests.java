0bdbb62d43fc256cebbfcbadd199823aeaecd49a
Sidaoui <mssidaoui@gmail.com>
2023-02-24 20:43:52 +0100

    security_project_skeleton

---
+++ springSecurity_Project/src/test/java/com/sid/springSecurity_Project/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.springSecurity_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
